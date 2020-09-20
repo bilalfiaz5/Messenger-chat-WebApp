84e4f3997501ea475a383b35fdc4c6605ab56368
Bilal Fayyaz <33794813+bilalfiaz5@users.noreply.github.com>
2020-09-20 11:06:40 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,7 @@
 import firebase from 'firebase'
 
 const firebaseApp =  firebase.initializeApp({
-    apiKey: "AIzaSyBf2pMFKiPtL3DweyjVKt68Xr8f018056c",
-    authDomain: "messenger-web-app.firebaseapp.com",
-    databaseURL: "https://messenger-web-app.firebaseio.com",
-    projectId: "messenger-web-app",
-    storageBucket: "messenger-web-app.appspot.com",
-    messagingSenderId: "833919277874",
-    appId: "1:833919277874:web:ce9d8cac89f0748f94952b",
-    measurementId: "G-ZSLEG1G2D1"
+  //paste the firebase config inside the {} brackets here!!!!!
   
 });
 const db =firebaseApp.firestore();
